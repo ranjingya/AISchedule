dcf3c4e10d4f1a1d69f770dea4dd830f80ffbc5f
Ranjyaa <2061522309@qq.com>
2024-03-22 00:32:40 +0800

    发现并添加了一个神奇的查询个人课表的入口

--- provider.js
+++ provider.js
@@ -1,16 +1,19 @@
 async function scheduleHtmlProvider(dom = document) {
     await loadTool('AIScheduleTools')
     const url = "http://jwxt.just.edu.cn/jwglxt/kbcx/xskbcx_cxXskbcxIndex.html?gnmkdm=N2151&layout=default"
+    const url2 = "http://jwxt.just.edu.cn/jwglxt/kbcx/xskbcx_cxXskbcxIndex.html?gnmkdm=N253508&layout=default"
     const url_v = "https://client.v.just.edu.cn/http/webvpn12738b7746cb46ff465fb0bca782b8a72738b83605805ff098e4d664058cd8ed/jwglxt/kbcx/xskbcx_cxXskbcxIndex.html?gnmkdm=N2151&layout=default"
     var currentURL = window.location.href
 
     try {
-        if (currentURL === url || currentURL === url_v) {
+        if (currentURL === url || currentURL === url_v || currentURL === url2) {
             return  dom.querySelector('#table1').outerHTML;
         } else {
             await AIScheduleAlert({
-                titleText: '当前页面错误', // 标题内容，字体比较大，不传默认为提示
-                contentText: '检查当前页面，路径为：登陆后--信息查询--个人课表查询', // 提示信息，字体稍小，支持使用``达到换行效果，具体使用效果建议真机测试
+                titleText: '检查当前页面是否为课表界面', // 标题内容，字体比较大，不传默认为提示
+                contentText: `正确路径为：登陆后--菜单栏--信息查询--个人课表查询
+                
+                              或者：登陆后--菜单栏--选课--个人课表查询`, // 提示信息，字体稍小，支持使用``达到换行效果，具体使用效果建议真机测试
                 confirmText: '确认', // 确认按钮文字，可不传默认为确认
             })
             return 'do not continue'
